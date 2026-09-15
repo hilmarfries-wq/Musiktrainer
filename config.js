bdc14009ecf70e1b1fbb649a6e7d791fabca725c
hilmarfries-wq <hilmarfries@me.com>
2026-09-15 16:41:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.MUSIKTRAINER_CONFIG = {
 
   supabase: {
     enabled: true,
-    url: "https://owemgypenebvynskadgi.supabase.co/rest/v1/",
+    url: "https://owemgypenebvynskadgi.supabase.co",
     publishableKey: "sb_publishable_iL0iy85bWf-vJmWkrjoTqg_x8BjFzG7"
   }
 };
