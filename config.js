7155617909ff94e62bbd8512178bab3e26a29509
hilmarfries-wq <hilmarfries@me.com>
2026-09-15 16:19:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.MUSIKTRAINER_CONFIG = {
   appName: "Musiktrainer",
   appVersion: "3.3.0",
-  teacherPin: "2000",
+  teacherPin: "071124",
   storageMode: "local+supabase",
 
   supabase: {
-    enabled: false,
-    url: "https://DEIN-PROJEKT.supabase.co",
-    publishableKey: "DEIN_PUBLISHABLE_ODER_ANON_KEY"
+    enabled: true,
+    url: "https://owemgypenebvynskadgi.supabase.co/rest/v1/",
+    publishableKey: "sb_publishable_iL0iy85bWf-vJmWkrjoTqg_x8BjFzG7"
   }
 };
